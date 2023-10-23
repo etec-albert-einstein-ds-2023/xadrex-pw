fd9472360f3de162095ccb6cba5b539663eee5bc
Username-MB <lucasgodpassos@gmail.com>
2023-10-23 16:14:25 -0300

    fiz o dark mode, so tem q mudar o icone dele

--- javas.js
+++ javas.js
@@ -4,4 +4,8 @@ setTimeout(function abrirform() {
 function fechaform() {
     document.getElementById("form-box").style.display = "none"
 }
- 
+ function darkmode(){
+    let bg = document.body.style.background = "#333"
+    let text = document.getElementById("text1").style.color = "whitesmoke"
+    let title = document.getElementById("title").style.color = "whitesmoke"
+}
